6258db3bf1db53d8d3f227780350636a8f4405cc
charjofra <96026806+charjofra@users.noreply.github.com>
2026-02-23 01:50:04 +0000

    added is_staff attribute to User class

--- src/models/staff.py
+++ src/models/staff.py
@@ -1,6 +1,6 @@
 from .user import User
 
 class Staff(User):
-    def __init__(self, user_id: int, location_id: int, full_name: str, email: str, password_hash: str, created_at: float, is_active: bool, role: str) -> None:
-        super().__init__(user_id, location_id, full_name, email, password_hash, created_at, is_active)
+    def __init__(self, user_id: int, location_id: int, full_name: str, email: str, password_hash: str, is_staff: bool, created_at: float, is_active: bool, role: str) -> None:
+        super().__init__(user_id, location_id, full_name, email, password_hash, is_staff, created_at, is_active)
         self.role = role
--- src/models/tenant.py
+++ src/models/tenant.py
@@ -1,8 +1,8 @@
 from .user import User
 
 class Tenant(User):
-    def __init__(self, user_id: int, location_id: int, full_name: str, email: str, password_hash: str, created_at: float, is_active: bool, tenant_id: int, ni_number: str, occupation: str, references_txt: str, requirements: str) -> None:
-        super().__init__(user_id, location_id, full_name, email, password_hash, created_at, is_active)
+    def __init__(self, user_id: int, location_id: int, full_name: str, email: str, password_hash: str, is_staff: bool, created_at: float, is_active: bool, tenant_id: int, ni_number: str, occupation: str, references_txt: str, requirements: str) -> None:
+        super().__init__(user_id, location_id, full_name, email, password_hash, is_staff, created_at, is_active)
         self.tenant_id = tenant_id
         self.ni_number = ni_number
         self.occupation = occupation
