2e79ddb1327491f520ddf2dca0f4f2eee6edcdc3
BLEF <121230919+BLEF72@users.noreply.github.com>
2026-04-01 14:21:37 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "port": int(os.getenv("DB_PORT", "3306")),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", ""),
+    "password": os.getenv("DB_PASSWORD", "123456789"),
     "database": os.getenv("DB_NAME", "paragon_apartment_management_system"),
 }
 
