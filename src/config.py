99e6523c1f8a50c5ed5dcc0828528598cdbcc6db
Abrar Lone <abrarlone01@outlook.com>
2026-04-01 14:30:21 +0100

    Revert "Update config.py"
    This reverts commit 2e79ddb1327491f520ddf2dca0f4f2eee6edcdc3.

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "port": int(os.getenv("DB_PORT", "3306")),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "123456789"),
+    "password": os.getenv("DB_PASSWORD", ""),
     "database": os.getenv("DB_NAME", "paragon_apartment_management_system"),
 }
 
